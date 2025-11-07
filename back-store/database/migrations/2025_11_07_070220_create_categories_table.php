3b918bab0d5e9c9075700925aab65c902d2fa8e1
GP-Bun <caca.ngoc2@gmail.com>
2025-11-07 19:44:08 +0700

    cập nhật categories

--- back-store/database/migrations/2025_11_07_070220_create_categories_table.php
+++ back-store/database/migrations/2025_11_07_070220_create_categories_table.php
@@ -9,14 +9,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->text('description')->nullable();
-            $table->string('image')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        $table->id();
+        $table->string('name');
+        $table->text('description')->nullable();
+        $table->timestamps();
+        $table->softDeletes();
+    });
     }
 
     public function down(): void
