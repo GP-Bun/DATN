9cb82e64e21551bc018b05cccca3b9864734d0b2
GP-Bun <caca.ngoc2@gmail.com>
2025-12-09 03:27:04 +0700

    call api của home

--- th-store/src/api/api.ts
+++ th-store/src/api/api.ts
@@ -5,13 +5,13 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api", 
 });
 
-// (Tùy chọn) Nếu bạn có token đăng nhập:
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+// Thêm token vào header nếu user đã đăng nhập
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("user_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
